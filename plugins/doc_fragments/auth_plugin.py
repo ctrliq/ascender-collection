970dc1424692c3e8517c6b79e217e62ec7b12812
blai <blaipr@hotmail.com>
2026-06-26 23:33:10 +0200

    Modernize doc fragments, fix stale Tower comments, and update deprecated import
    - Remove old-style (object) class inheritance in auth.py and auth_plugin.py
      doc fragments, using implicit object inheritance (Python 3)
    - Replace stale "Tower" references in license.py and subscriptions.py comments
      with "controller"
    - Update deprecated ansible.module_utils._text import path to
      ansible.module_utils.common.text.converters in controller_object_diff lookup

--- plugins/doc_fragments/auth_plugin.py
+++ plugins/doc_fragments/auth_plugin.py
@@ -2,7 +2,7 @@
 # Copyright: (c) 2020, Ansible by Red Hat, Inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-class ModuleDocFragment(object):
+class ModuleDocFragment:
 
     # Automation Platform Controller documentation fragment
     DOCUMENTATION = r'''
