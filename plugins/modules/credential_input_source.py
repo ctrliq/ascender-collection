554f9c8eae06365c3b6b5e1b4984a7f5f5030827
blai <blaipr@hotmail.com>
2026-06-26 11:17:15 +0200

    Remove deprecated ANSIBLE_METADATA from all modules

--- plugins/modules/credential_input_source.py
+++ plugins/modules/credential_input_source.py
@@ -3,8 +3,6 @@
 # Copyright: (c) 2020, Tom Page <tpage@redhat.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-ANSIBLE_METADATA = {'metadata_version': '1.1', 'status': ['preview'], 'supported_by': 'community'}
-
 DOCUMENTATION = '''
 ---
 module: credential_input_source
